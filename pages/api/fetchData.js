6d108063d04d5bc4e49497e1348ed9b98e1f7e64
phantomDD92 <lovelymonkey0612@gmail.com>
2024-03-30 07:04:21 -0400

    add api route

---
+++ pages/api/fetchData.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  try {
+    const apiUrl =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
