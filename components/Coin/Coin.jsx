008fc0ef7f0b685aa5cec6a0a034786cee76c671
phantomDD92 <lovelymonkey0612@gmail.com>
2024-03-30 07:35:54 -0400

    optimize

--- components/Coin/Coin.jsx
+++ components/Coin/Coin.jsx
@@ -1,42 +1,38 @@
-import Link from 'next/link'
-import styles from './Coin.module.css'
-
+import Link from "next/link";
+import styles from "./Coin.module.css";
 
 function Coin(props) {
+  const { id, name, price, symbol, image, volume, priceChange } = props;
 
-    const { id, name, price, symbol, image, volume, priceChange } = props
+  return (
+    <Link href={`/coin/${id}`}>
+      <a>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={`${image}`} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>${price}</p>
+              <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
 
-    return (
-        <Link href={`/coin/${id}`}>
-            <a>
-                <div className={styles.coin_container}>
-                    <div className={styles.coin_row}>
-                        <div className={styles.coin}>
-                            <img src={`${image}`} alt={name} className={styles.coin_img} />
-                            <h1 className={styles.coin_h1}>{name}</h1>
-                            <p className={styles.coin_symbol}>{symbol}</p>
-                        </div>
-                        <div className={styles.coin_data}>
-                            <p className={styles.coin_price}>${price}</p>
-                            <p className={styles.coin_volume}>${volume.toLocaleString()}
-                            </p>
-
-                            {priceChange < 0 ? (
-                                <p className={(styles.coin_percent, styles.red)}>
-                                    {priceChange.toFixed(2)}%
-                                </p>
-                            ) : (
-                                <p className={(styles.coin_percent, styles.green)}>
-                                    {priceChange.toFixed(2)}%
-                                </p>
-                            )}
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </Link>
-    )
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
 }
 
-export default Coin
-
+export default Coin;
